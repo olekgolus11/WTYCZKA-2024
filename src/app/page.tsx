b550a20e10bceecf5019262c38255b395514056a
Gujansko <mateusz.klos150401@gmail.com>
2023-09-04 09:33:19 +0200

    Main Page layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import Clock from "@/components/Clock/Clock";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Clock />
-      <button className="button-square button-filled">Click me</button>
-      <button className="button-round button-outlined">Click me</button>
-    </div>
+    <main className="flex flex-wrap items-center justify-around h-full">
+      <section>
+        <Clock />
+      </section>
+      <section>
+        <div className="flex flex-wrap gap-4 items-center justify-center">
+          <button className="button-round button-filled">Click me</button>
+          <button className="button-round button-outlined">Click me</button>
+        </div>
+      </section>
+    </main>
   );
 }
