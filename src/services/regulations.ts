f1c260ae1bfd425dab51419ab7a5710220ced966
olekgolus11 <olekgolus02@gmail.com>
2024-09-25 17:18:57 +0200

    Update regulations and camp statement urls

--- src/services/regulations.ts
+++ src/services/regulations.ts
@@ -1,2 +1,2 @@
 export const REGULATIONS =
-  "https://firebasestorage.googleapis.com/v0/b/wtyczka-b0ddc.appspot.com/o/files%2Foswiadczenie.docx?alt=media&token=ba8db071-a04e-4039-8030-a9d3fbd3397d";
+  "https://firebasestorage.googleapis.com/v0/b/wtyczka-b0ddc.appspot.com/o/files%2Fregulamin.docx?alt=media&token=4f0dca46-d838-4959-b307-c605bc458494";
