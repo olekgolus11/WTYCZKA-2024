4bf2afd8e52b7abe59376b0ab83454b12ef8441d
olekgolus11 <olekgolus02@gmail.com>
2024-09-26 16:51:25 +0200

    Update information on registry

--- src/components/RegistrationForm/ParticipantInfoSection.tsx
+++ src/components/RegistrationForm/ParticipantInfoSection.tsx
@@ -9,6 +9,16 @@ const ParticipantInfoSection = () => {
   const { languageMode } = useLanguageModeContext();
   return (
     <>
+      <Typography variant="h6" className=" p-4">
+        {languageMode == "english"
+          ? `If a minor provides false information, they will be disqualified from participating in this and future editions of the "Wtyczka" and will not be refunded.`
+          : `Jeśli osoba niepełnoletnia poda nieprawdziwe informacje, zostanie ona pozbawiona możliwości uczestniczenia w tej, jak i w kolejnych edycjach "Wtyczki" oraz nie dostanie zwrotu kosztów` }
+      </Typography>
+      <Typography variant="h6" className=" p-4">
+        {languageMode == "english"
+          ? `Data on student status will be verified and if found to be incorrect, it will result in disqualification from participation in this and future editions of the "Wtyczka".`
+          : `Dane o posiadaniu statusu studenta będą weryfikowane i jeśli okażą się niepoprawne, będzie się to wiązało z wykluczeniem z udziału w tej oraz kolejnych edycjach "Wtyczki".`}
+      </Typography>
       <Typography variant="h6" className="text-primary-color p-4">
         {languageMode == "english"
           ? "Participant information"
@@ -28,6 +38,14 @@ const ParticipantInfoSection = () => {
           isRequired={true}
           minLength={3}
           maxLength={15}
+        />
+        <FormField
+          label={languageMode == "english" ? "Date of birth" : "Data urodzenia"}
+          registerName="birthDate"
+          isRequired={true}
+          fieldType="date"
+          minLength={10}
+          maxLength={10}
         />
         <FormField
           label={languageMode == "english" ? "E-mail adress" : "Adres e-mail"}
@@ -58,14 +76,6 @@ const ParticipantInfoSection = () => {
           registerName="sex"
           isRequired={true}
           options={languageMode == "english" ? SexOptions.EN : SexOptions.PL}
-        />
-        <FormField
-          label={languageMode == "english" ? "Date of birth" : "Data urodzenia"}
-          registerName="birthDate"
-          isRequired={true}
-          fieldType="date"
-          minLength={10}
-          maxLength={10}
         />
       </div>
     </>
