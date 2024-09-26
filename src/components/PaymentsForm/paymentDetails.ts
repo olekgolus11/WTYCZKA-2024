2437f2b9240d83f3c78967ea77d547709e5b7ff8
olekgolus11 <olekgolus02@gmail.com>
2024-09-26 18:13:53 +0200

    update price

--- src/components/PaymentsForm/paymentDetails.ts
+++ src/components/PaymentsForm/paymentDetails.ts
@@ -2,7 +2,7 @@ export const PaymentDetailsTextPL = {
   transferRecipient: "Odbiorca płatności: Wydział Elektrotechniki, Elektroniki, Informatyki i Automatyki",
   accountNumber: "Numer konta do odbiorcy: 03 1240 3028 1111 0010 3741 8675",
   title: "Tytuł przelewu: WTYCZKA2024-ImieNazwisko",
-  amount: "Kwota: 400 zł (+ 30zł jeśli dieta wegetariańska)",
+  amount: "Kwota: 450 zł (+ 30zł jeśli dieta wegetariańska)",
 };
 
 export const PaymentDetailsTextEN = {
@@ -10,5 +10,5 @@ export const PaymentDetailsTextEN = {
     "Transfer recipient: Faculty of Electrical, Electronic, Computer and Control Engineering",
   accountNumber: "Recipient's account number: 03 1240 3028 1111 0010 3741 8675",
   title: "Title of the transfer: WTYCZKA2024-FirstnameLastname",
-  amount: "Amount: 400 zł (+ 30zł for vegetarian diet)",
+  amount: "Amount: 450 zł (+ 30zł for vegetarian diet)",
 };
