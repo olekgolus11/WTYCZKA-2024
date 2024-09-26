1245d83173506bbc6df9594ade08725f6331e0f3
olekgolus11 <olekgolus02@gmail.com>
2024-09-26 17:57:51 +0200

    Update regulations

--- src/services/regulations.ts
+++ src/services/regulations.ts
@@ -1,2 +1,2 @@
 export const REGULATIONS =
-  "https://firebasestorage.googleapis.com/v0/b/wtyczka-b0ddc.appspot.com/o/files%2Fregulamin.docx?alt=media&token=4f0dca46-d838-4959-b307-c605bc458494";
+  "https://firebasestorage.googleapis.com/v0/b/wtyczka-b0ddc.appspot.com/o/files%2Fregulamin.pdf?alt=media&token=05493352-82ca-4d0b-8daf-c199e41b6c2f";
