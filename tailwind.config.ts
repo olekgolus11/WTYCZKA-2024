417b2016d79fc0a6487e617dece2f0d22b80da1d
Olek Golus <olekgolus02@gmail.com>
2024-09-29 15:42:13 +0200

    Add data viewer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,54 +1,103 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontSize: {
-        "small-label": "2rem",
-        "bigger-label": "2.5rem",
-      },
-      colors: {
-        "primary-color": "#d21212",
-        "secondary-color": "#b00d0d",
-        "active-color": "#aa1313",
-        "mobile-grey": "#050505",
-      },
-      stroke: {
-        "primary-color": "#d21212",
-        "secondary-color": "#b00d0d",
-        "active-color": "#aa1313",
-      },
-      borderColor: {
-        "primary-color": "#d21212",
-        "secondary-color": "#b00d0d",
-        "active-color": "#aa1313",
-      },
-      boxShadowColor: {
-        "primary-color": "#d21212",
-        "secondary-color": "#b00d0d",
-        "active-color": "#aa1313",
-      },
-      keyframes: {
-        "spring-right": {
-          from: { transform: "translateX(0px)" },
-          to: { transform: "translateX(6px)" },
-        },
-      },
-      animation: {
-        "spring-right": "spring-right 0.5s ease-in-out infinite alternate",
-      },
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		fontSize: {
+  			'small-label': '2rem',
+  			'bigger-label': '2.5rem'
+  		},
+  		colors: {
+  			'primary-color': '#d21212',
+  			'secondary-color': '#b00d0d',
+  			'active-color': '#aa1313',
+  			'mobile-grey': '#050505',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		stroke: {
+  			'primary-color': '#d21212',
+  			'secondary-color': '#b00d0d',
+  			'active-color': '#aa1313'
+  		},
+  		borderColor: {
+  			'primary-color': '#d21212',
+  			'secondary-color': '#b00d0d',
+  			'active-color': '#aa1313'
+  		},
+  		boxShadowColor: {
+  			'primary-color': '#d21212',
+  			'secondary-color': '#b00d0d',
+  			'active-color': '#aa1313'
+  		},
+  		keyframes: {
+  			'spring-right': {
+  				from: {
+  					transform: 'translateX(0px)'
+  				},
+  				to: {
+  					transform: 'translateX(6px)'
+  				}
+  			}
+  		},
+  		animation: {
+  			'spring-right': 'spring-right 0.5s ease-in-out infinite alternate'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
