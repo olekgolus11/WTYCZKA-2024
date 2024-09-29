4577e9d33cb97d45ec80792270272f20f6bccf8a
Olek Golus <olekgolus02@gmail.com>
2024-09-29 15:42:13 +0200

    Add data viewer

---
+++ src/app/viewer/page.tsx
@@ -0,0 +1,200 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/config/firebase";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    getPaginationRowModel,
+    getSortedRowModel,
+    SortingState,
+    getFilteredRowModel,
+    ColumnFiltersState,
+} from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ArrowUpDown } from "lucide-react";
+
+type registrationType = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    pesel: string;
+    sex: string;
+    faculty: string;
+    indexNumber: string;
+    degree: string;
+    collegeLevel: string;
+    fieldOfStudy: string;
+    diet: string;
+    shirtSize: string;
+    source: string;
+    invoice: string;
+    statuteAccept: boolean;
+    personalDataAccept: boolean;
+    additionalAccept: boolean;
+    firstNameInvoice: string;
+    lastNameInvoice: string;
+    nipPeselInvoice: string;
+    sourceOther: string;
+    maturityConsent: boolean;
+    birthDate?: string;
+};
+
+const columns: ColumnDef<registrationType>[] = [
+    {
+        accessorKey: "firstName",
+        header: ({ column }) => {
+            return (
+                <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    First name
+                    <ArrowUpDown className='ml-2 h-4 w-4' />
+                </Button>
+            );
+        },
+    },
+    {
+        accessorKey: "lastName",
+        header: ({ column }) => {
+            return (
+                <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Last name
+                    <ArrowUpDown className='ml-2 h-4 w-4' />
+                </Button>
+            );
+        },
+    },
+    {
+        accessorKey: "email",
+        header: "Email",
+    },
+    {
+        accessorKey: "phone",
+        header: "Phone",
+    },
+    {
+        accessorKey: "faculty",
+        header: "Faculty",
+    },
+    {
+        accessorKey: "fieldOfStudy",
+        header: "Field of Study",
+    },
+    {
+        accessorKey: "degree",
+        header: "Degree",
+    },
+    {
+        accessorKey: "collegeLevel",
+        header: "College Level",
+    },
+];
+
+const ViewerPage = () => {
+    const [users, setUsers] = useState<registrationType[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "registration"));
+                const fetchedUsers: registrationType[] = [];
+                querySnapshot.forEach((doc) => {
+                    fetchedUsers.push(doc.data() as registrationType);
+                });
+                setUsers(fetchedUsers);
+                setLoading(false);
+            } catch (err) {
+                setError("Error fetching users");
+                setLoading(false);
+                console.error("Error fetching users:", err);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    const table = useReactTable({
+        data: users,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            sorting,
+            columnFilters,
+        },
+    });
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div className='p-4'>
+            <h1 className='text-2xl font-bold mb-4'>Registered Users</h1>
+            <div className='flex items-center py-4'>
+                <Input
+                    placeholder='Filter emails...'
+                    value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) => table.getColumn("email")?.setFilterValue(event.target.value)}
+                    className='max-w-sm'
+                />
+            </div>
+            <div className='rounded-md border'>
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className='h-24 text-center'>
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className='flex items-center justify-end space-x-2 py-4'>
+                <Button onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                    Previous
+                </Button>
+                <Button onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                    Next
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default ViewerPage;
