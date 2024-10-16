9511367c4948f4169b391d30d2ab968e53303c42
Misiek22113 <miskojakub@gmail.com>
2024-10-16 18:45:20 +0200

    change password

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers'
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function POST(req: Request) {
   const { password } = await req.json();
-  const cookiesStore = cookies()
+  const cookiesStore = cookies();
 
-  if (password === 'omerta') {
-    console.log('[ACTION] Password is correct');
-    cookiesStore.set('magic', 'test');
+  if (password === "syndykat") {
+    console.log("[ACTION] Password is correct");
+    cookiesStore.set("magic", "test");
     return NextResponse.json({ success: true });
   }
 
-  console.log('[ACTION] Password is incorrect!!!');
+  console.log("[ACTION] Password is incorrect!!!");
 
   return NextResponse.json({ success: false }, { status: 401 });
 }
